99d494c17db40637225854a155c40a6fb0c1ea37
kunal <kunalanagpure@gmail.com>
2022-08-06 22:46:56 +0530

    release-1.a commit

---
+++ b2.py
@@ -0,0 +1,16 @@
+POST /edit/ HTTP/1.1
+Host: example.org
+User-Agent: Thingio/1.0
+Authorization: Basic ZGFmZnk6c2VjZXJldA==
+Content-Type: application/atom+xml;type=entry
+Content-Length: nnn
+Slug: First Post
+
+<?xml version="1.0"?>
+<entry xmlns="http://www.w3.org/2005/Atom">
+  <title>Atom-Powered Robots Run Amok</title>
+  <id>urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a</id>
+  <updated>2003-12-13T18:30:02Z</updated>
+  <author><name>John Doe</name></author>
+  <content>Some text.</content>
+</entry>
