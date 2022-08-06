58c9bf416515d528165cf0d039d000226170ab20
kunal <kunalanagpure@gmail.com>
2022-08-06 21:40:25 +0530

    initial commit

---
+++ test.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+@app.route('/')
+
+def filecheck():
+
+def filewrite():
+    # Data to be written
+    dictionary = {
+        "name": "sathiyajith",
+        "rollno": 56,
+        "cgpa": 8.6,
+        "phonenumber": "9976770500"
+    }
+
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=4)
+
+    # Writing to sample.json
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
+
+
+def hello_world():
+    return 'hello test'
+if __name__ == '__main__':
+    app.run()
+
